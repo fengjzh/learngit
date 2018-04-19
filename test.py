cf2609eff034f90bd3d5f3faa64d142894bc5b97
fengjzh <dodowolf-zjf@163.com>
2018-04-19 16:41:55 +0800

    new file test.py and zhjf

---
+++ test.py
@@ -0,0 +1,14 @@
+#!/bin/python36
+# written by zhangjunfeng
+# 2018-04-19
+
+def triangles(max):
+    N=[1]
+    n=0
+    while n<max:
+        yield N
+        N.append(0)
+        N=[N[i-1] + N[i] for i in range(len(N))]
+        n=n+1
+for i in triangles(12):
+    print(len(i))
