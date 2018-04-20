75d5df231509eb614157742223171c37946d791c
fengjzh <dodowolf-zjf@163.com>
2018-04-20 11:37:13 +0800

    wrote a student file

---
+++ student.py
@@ -0,0 +1,11 @@
+#!/bin/python36
+class Student(object):
+    def __init__(self,name,score):
+        self.__name=name
+        self.__score=score
+
+    def print_score(self):
+        return '%s:%s' % (self.__name,self.__score)
+Lucy=Student("lucy li",98)
+a=Lucy.print_score()
+print(a)
